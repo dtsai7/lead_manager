2b680bcd06b5a2b8c11aa401fe35154af0f89094
Chi Shiun Tsai <david.tsai0811@gmail.com>
2020-04-12 17:01:00 +0800

    Integrate React Part II

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react_dom';
+
+
+class App extends React.Component {
+    render() {
+        return <h1 > React App < /h1>
+    }
+}
+
+ReactDOM.render( < App / > , document.getElementById('app'))
